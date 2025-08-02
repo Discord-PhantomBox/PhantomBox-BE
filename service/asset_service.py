626c31c27b06ce69f7d1e14bdb62fb519d26a1b1
pdh0128 <pdh0128a@gmail.com>
2025-08-03 03:59:25 +0900

    fix :: 시그니쳐 오류 에러

--- service/asset_service.py
+++ service/asset_service.py
@@ -5,7 +5,7 @@ import asyncio
 async def select(label_id : int , asset_request : TextRequest):
     text = asset_request.text
     assets = await llm_util.select_assets(label_id, text)
-    tasks = [s3_util.get_presigned_urls_for_folder(folder) for folder in assets]
+    tasks = [s3_util.get_public_urls_for_folder(folder) for folder in assets]
     results = await asyncio.gather(*tasks)
     all_urls = [url for sublist in results for url in sublist]
     return all_urls
