98f143bde29d9f13333b5a6d0cdfa539e477367f
pdh0128 <pdh0128a@gmail.com>
2025-08-03 04:11:31 +0900

    feat :: 계층구조

--- service/asset_service.py
+++ service/asset_service.py
@@ -8,4 +8,20 @@ async def select(label_id : int , asset_request : TextRequest):
     tasks = [s3_util.get_public_urls_for_folder(folder) for folder in assets]
     results = await asyncio.gather(*tasks)
     all_urls = [url for sublist in results for url in sublist]
-    return all_urls
+    response = await nest_file_structure(all_urls)
+    return response
+
+async def nest_file_structure(flat_list: list[dict]) -> dict:
+    root = {}
+
+    for item in flat_list:
+        path_parts = item["file"].split("/")
+        current = root
+
+        for part in path_parts[:-1]:  # 디렉토리 부분만 순회
+            current = current.setdefault(part, {})
+
+        # 마지막 파일명을 URL로 연결
+        current[path_parts[-1]] = item["url"]
+
+    return root
